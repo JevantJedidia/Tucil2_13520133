fda05abb99e85dbeaf917b3719834c4d678293f3
JevantJedidia <73844363+JevantJedidia@users.noreply.github.com>
2022-02-27 10:42:58 +0700

    Add all visualization

--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from MyConvexHull import ConvexHull
 
-#Driver untuk data Sepal Width vs Sepal Length
+#Visualisasi data iris
 data = datasets.load_iris()
 #Buat DataFrame
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = pd.DataFrame(data.target)
 
+#Visualisasi Sepal Width vs Sepal Length
 plt.figure(figsize = (10, 6))
 colors = ['b','r','g']
 plt.title('Sepal Width vs Sepal Length')
@@ -18,6 +19,100 @@ plt.ylabel(data.feature_names[1])
 for i in range(len(data.target_names)):
     bucket = df[df['Target'] == i]
     bucket = bucket.iloc[:,[0,1]].values
+    hull = ConvexHull(bucket) #Penggunaan pustaka MyConvexHull
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    for simplex in hull:
+        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
+plt.legend()
+plt.show()
+
+#Visualisasi Petal Width vs Petal Length
+plt.figure(figsize = (10, 6))
+colors = ['b','r','g']
+plt.title('Petal Width vs Petal Length')
+plt.xlabel(data.feature_names[2])
+plt.ylabel(data.feature_names[3])
+for i in range(len(data.target_names)):
+    bucket = df[df['Target'] == i]
+    bucket = bucket.iloc[:,[2,3]].values
+    hull = ConvexHull(bucket) #Penggunaan pustaka MyConvexHull
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    for simplex in hull:
+        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
+plt.legend()
+plt.show()
+
+
+#Visualisasi data wine
+data = datasets.load_wine()
+#Buat DataFrame
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['Target'] = pd.DataFrame(data.target)
+
+#Visualisasi Malic Acid vs Alcohol
+plt.figure(figsize = (10, 6))
+colors = ['b','r','g']
+plt.title('Malic Acid vs Alcohol')
+plt.xlabel(data.feature_names[0])
+plt.ylabel(data.feature_names[1])
+for i in range(len(data.target_names)):
+    bucket = df[df['Target'] == i]
+    bucket = bucket.iloc[:,[0,1]].values
+    hull = ConvexHull(bucket) #Penggunaan pustaka MyConvexHull
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    for simplex in hull:
+        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
+plt.legend()
+plt.show()
+
+#Visualisasi Total Phenols vs Magnesium
+plt.figure(figsize = (10, 6))
+colors = ['b','r','g']
+plt.title('Total Phenols vs Magnesium')
+plt.xlabel(data.feature_names[4])
+plt.ylabel(data.feature_names[5])
+for i in range(len(data.target_names)):
+    bucket = df[df['Target'] == i]
+    bucket = bucket.iloc[:,[4,5]].values
+    hull = ConvexHull(bucket) #Penggunaan pustaka MyConvexHull
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    for simplex in hull:
+        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
+plt.legend()
+plt.show()
+
+
+#Visualisasi data breast_cancer
+data = datasets.load_breast_cancer()
+#Buat DataFrame
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['Target'] = pd.DataFrame(data.target)
+
+#Visualisasi Area vs Perimeter
+plt.figure(figsize = (10, 6))
+colors = ['b','r','g']
+plt.title('Area vs Perimeter')
+plt.xlabel(data.feature_names[2])
+plt.ylabel(data.feature_names[3])
+for i in range(len(data.target_names)):
+    bucket = df[df['Target'] == i]
+    bucket = bucket.iloc[:,[2,3]].values
+    hull = ConvexHull(bucket) #Penggunaan pustaka MyConvexHull
+    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
+    for simplex in hull:
+        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
+plt.legend()
+plt.show()
+
+#Visualisasi Compactness vs Smoothness
+plt.figure(figsize = (10, 6))
+colors = ['b','r','g']
+plt.title('Compactness vs Smoothness')
+plt.xlabel(data.feature_names[4])
+plt.ylabel(data.feature_names[5])
+for i in range(len(data.target_names)):
+    bucket = df[df['Target'] == i]
+    bucket = bucket.iloc[:,[4,5]].values
     hull = ConvexHull(bucket) #Penggunaan pustaka MyConvexHull
     plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
     for simplex in hull:
